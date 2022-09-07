0eaf056eff82a2811700992ddc1a8618c2357508
GhostCrab <Ryan.Pielow@gmail.com>
2022-09-07 16:01:39 -0700

    update

--- src/app/core/services/application-state.service.ts
+++ src/app/core/services/application-state.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApplicationStateService {
   private isMobileResolution: boolean;
+  private ismobileResolution$: BehaviorSubject<boolean>;
 
   constructor() {
     if (window.innerWidth <= 768) {
@@ -12,9 +14,15 @@ export class ApplicationStateService {
     } else {
       this.isMobileResolution = false;
     }
+
+    this.ismobileResolution$ = new BehaviorSubject<boolean>(this.isMobileResolution);
   }
 
   public getIsMobileResolution(): boolean {
     return this.isMobileResolution;
+  }
+
+  public watchIsMobileResolution(): Observable<boolean> {
+    return this.ismobileResolution$.asObservable();
   }
 }
